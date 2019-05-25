628d3cb3e91d7b3caf7b1b5e7cdec38d635d3fd2
Aaron Johnson <aaron.johnson1031@gmail.com>
2019-05-24 22:14:32 -0400

    created functions on callbacks.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,6 +1,6 @@
 // Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const items = ["Pencil", "Notebook", "yo-yo", "Gum"];
 
 /* 
 
@@ -24,32 +24,74 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 */
 
-
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+
+getLength(items, len => {
+  console.log(`Length of array 'items' is: ${len}`);
+});
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 }
+
+last(items, x => {
+  console.log(`Last item in array 'items' is: ${x}`);
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
+
+sumNums(5, 10, sum => {
+  console.log(`The sum of the two numbers passed in is: ${sum}`);
+});
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
+
+multiplyNums(5, 10, product => {
+  console.log(`The product of the two numbers passed in is: ${product}`);
+});
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  return cb(list.includes(item));
 }
 
-/* STRETCH PROBLEM */
+contains("Notebook", items, has => {
+  console.log(has);
+});
 
+/* STRETCH PROBLEM */
+const dupeArray = [
+  "chew",
+  "toy",
+  "dog",
+  "bowl",
+  "fleas",
+  "dog",
+  "toy",
+  "food",
+  "water"
+];
 function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  let arr = [];
+  return cb(
+    array.filter(item => (!arr.includes(item) ? arr.push(item) : null))
+  );
 }
+
+removeDuplicates(dupeArray, x => {
+  console.log(x);
+});
